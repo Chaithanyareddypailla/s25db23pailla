591ea37b672ef7e3c2b7d09e295e69d77cac8160
Chaithanyareddypailla <chaithanyareddypailla@gmail.com>
2025-04-08 17:31:09 -0500

    added files

--- models/chimes.js
+++ models/chimes.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const chimesSchema = mongoose.Schema({
   chime_type: String,
+  material: String,  // <-- Add material field here to match your Pug
   size: String,
   cost: Number
 });
