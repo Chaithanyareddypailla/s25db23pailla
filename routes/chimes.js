c6dea67dea9e9ff6acb5d0e84d59aa4f1b247da2
Chaithanyareddypailla <chaithanyareddypailla@gmail.com>
2025-04-08 21:14:57 -0500

    added files

--- routes/chimes.js
+++ routes/chimes.js
@@ -7,4 +7,10 @@ router.get('/', function(req, res, next) {
 
 module.exports = router;
 
+var express = require('express');
+const chimes_controlers= require('../controllers/chimes');
+var router = express.Router();
+/* GET chimes */
+router.get('/', chimes_controllers.chimes_view_all_Page );
+module.exports = router;
 
